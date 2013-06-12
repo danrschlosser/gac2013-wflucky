9fe8ee811deb4d22bda2fa228c81280183027251
Doug Christman <DouglasChristman@gmail.com>
2013-06-12 12:14:36 -0700

    XML layout stuffs

--- BopWithFriends/src/main/java/com/wflucky/bopwithfriends/Main.java
+++ BopWithFriends/src/main/java/com/wflucky/bopwithfriends/Main.java
@@ -1,8 +1,17 @@
 package com.wflucky.bopwithfriends;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends Activity {
 
@@ -12,6 +21,65 @@ public class Main extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void joinGame(View view) {
+        setContentView(R.layout.activity_lobby);
+        Button button=(Button)findViewById(R.id.button);
+        button.setVisibility(View.INVISIBLE);
+    }
+
+    public void hostGame(View view) {
+
+        /////////////DBC//////////////
+        final CharSequence[] items = {"Hot Potato", "Sudden Death"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick a game mode");
+        builder
+        .setNegativeButton("Hot Potato", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                setContentView(R.layout.activity_lobby);
+                TextView textView = (TextView)findViewById(R.id.textView);
+                textView.setText(getName() + "'s Hot Potato Game");
+            }
+        })
+        .setPositiveButton("Sudden Death", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                setContentView(R.layout.activity_lobby);
+                TextView textView = (TextView)findViewById(R.id.textView);
+                textView.setText(getName() + "'s Sudden Death Game");
+            }
+        })
+        .create().show();
+        /////////////DBC//////////////
+    }
+
+    private String getName() {
+        String name = "Host";
+
+        try {
+            Cursor c = this.getContentResolver().query(ContactsContract.Profile.CONTENT_URI, null, null, null, null);
+            int count = c.getCount();
+            String[] columnNames = c.getColumnNames();
+            boolean b = c.moveToFirst();
+            int position = c.getPosition();
+            if (count == 1 && position == 0) {
+                for (int j = 0; j < columnNames.length; j++) {
+                    String columnName = columnNames[j];
+                    String columnValue = c.getString(c.getColumnIndex(columnName));
+
+                    if (columnName != null && columnName.equals("display_name")) {
+                        name = columnValue != null ? columnValue : "Host";
+                        break;
+                    }
+                }
+            }
+
+            c.close();
+
+            name = name.split(" ")[0];
+            } catch (Exception e) {}
+
+        return name;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
