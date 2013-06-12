704b1bdf6af20f68f287fe410b8dbe7e723dd3af
Anna Skorodumova <SkorAnna@gmail.com>
2013-06-12 16:34:00 -0700

    I am awesome

---
+++ BopWithFriends/src/main/java/com/wflucky/bopwithfriends/Scoreboard.java
@@ -0,0 +1,20 @@
+package com.wflucky.bopwithfriends;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.HashMap;
+
+/**
+ * Created by Dan on 6/11/13.
+ */
+public class Scoreboard extends Activity {
+
+    public Scoreboard(HashMap<Integer, String> playerRanks){
+
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
