1b7b86b515158affc09b78574546ec0a900fd41a
Andrew Fang <andrewbrianfang@gmail.com>
2013-06-11 18:17:04 -0700

    Added defs to Game

--- BopWithFriends/src/main/java/com/wflucky/bopwithfriends/Game.java
+++ BopWithFriends/src/main/java/com/wflucky/bopwithfriends/Game.java
@@ -4,7 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * Created by Dan on 6/11/13.
+ * Created by Andrew.
+ * Put an X once you've figured out how to do it, and put the method in this class.
+ * Gestures:
+ * [ ] Tap Once
+ * [ ] Tap Twice
+ * [ ] Pinch in
+ * [ ] Pinch out
+ * [ ] Swipe up
+ * [ ] Swipe down
+ * [ ] Swipe up two fingers
+ * [ ] Swipe down two fingers
+ * Motions:
+ * [ ] Shake
+ * [ ] Twist
+ * [ ] Pull
+ * [ ] Flip
+ * [ ] Raise
+ * [ ] Level
  */
 public class Game extends Activity {
     public void onCreate(Bundle savedInstanceState) {
