2cc45379fdb9c7846b5a6b520264145fa1de9a80
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-06-11 17:51:13 -0700

    Created the project

---
+++ BopWithFriends/src/main/java/com/wflucky/bopwithfriends/Lobby.java
@@ -0,0 +1,13 @@
+package com.wflucky.bopwithfriends;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by demouser on 6/11/13.
+ */
+public class Lobby extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
